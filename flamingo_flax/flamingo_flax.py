4f31c09fdf229794af3416a0ccf239cf815d7918
Enrico Shippole <henryshippole@gmail.com>
2022-06-20 13:43:02 -0400

    Add files via upload

--- flamingo_flax/flamingo_flax.py
+++ flamingo_flax/flamingo_flax.py
@@ -1,14 +1,29 @@
 import flax.linen as nn
 
+import math
+
+from functools import wraps
+
 import jax
 import jax.numpy as jnp
 from jax.numpy import einsum
 
 from einops import rearrange, repeat
-from einops_exts import rearrange_many, repeat_many
+#from einops_exts import rearrange_many, repeat_many
 
 def exists(val):
     return val is not None
+
+def masked_fill_(t, mask, value):
+    return t * (1 - mask) + value * mask
+
+def _many(fn):
+    @wraps(fn)
+    def inner(tensors, pattern, **kwargs):
+        return (fn(tensor, pattern, **kwargs) for tensor in tensors)
+    return inner
+
+rearrange_many = _many(rearrange)
 
 class FeedForward(nn.Module):
     dim: int
@@ -29,7 +44,7 @@ class PerceiverAttention(nn.Module):
     heads: int = 8
 
     @nn.compact
-    def forward(self, x, latents):
+    def __call__(self, x, latents):
 
         scale = self.dim_head ** -0.5
         heads = self.heads
@@ -47,7 +62,7 @@ class PerceiverAttention(nn.Module):
 
         b, m, h = *x.shape[:2], self.heads
 
-        q = nn.Dense(features = self.inner_dim, use_bias = False)(latents)
+        q = nn.Dense(features = inner_dim, use_bias = False)(latents)
 
         # the paper differs from Perceiver in which they also concat the key / values derived from the latents to be attended to
         kv_input = jnp.concatenate((x, latents), axis = -2)
@@ -61,7 +76,7 @@ class PerceiverAttention(nn.Module):
 
         sim = einsum('... i d, ... j d  -> ... i j', q, k)
 
-        sim = sim - jnp.amax(sim, axis = -1, keepdims = True).detach()
+        sim = sim - jnp.amax(sim, axis = -1, keepdims = True)
         attn = nn.softmax(sim, axis = -1)
 
         out = einsum('... i j, ... j d -> ... i d', attn, v)
@@ -80,25 +95,25 @@ class PerceiverResampler(nn.Module):
     @nn.compact
     def __call__(self, x):
 
-        self.latents = nn.Parameter(torch.randn(num_latents, dim))
-        self.time_pos_emb = nn.Parameter(torch.randn(num_time_embeds, 1, dim))
+        latents = jax.random.normal(key, [self.num_latents, self.dim])
+        time_pos_emb = jax.random.normal(key ,[self.num_time_embeds, 1, self.dim])
 
         layers = []
         for _ in range(self.depth):
-            layers.append(nn.ModuleList([
+            layers.append([
                 PerceiverAttention(dim = self.dim, dim_head = self.dim_head, heads = self.heads),
                 FeedForward(dim = self.dim, mult = self.ff_mult)
-            ]))
+            ])
 
         if x.ndim == 3:
             x = rearrange(x, 'b n d -> b 1 n d')
 
         times = x.shape[1]
-        x = x + self.time_pos_emb[:times]
+        x = x + time_pos_emb[:times]
 
-        latents = repeat(self.latents, 'n d -> b m n d', b = x.shape[0], m = x.shape[1])
+        latents = repeat(latents, 'n d -> b m n d', b = x.shape[0], m = x.shape[1])
 
-        for attn, ff in self.layers:
+        for attn, ff in layers:
             latents = attn(x, latents) + latents
             latents = ff(latents) + latents
 
@@ -139,7 +154,7 @@ class MaskedCrossAttention(nn.Module):
         sim = einsum('... i d, ... j d -> ... i j', q, k)
 
         if exists(media_locations):
-            text_time = media_locations.cumsum(dim = -1) # at each boolean of True, increment the time counter (relative to media time)
+            text_time = jnp.cumsum(media_locations, axis = -1) # at each boolean of True, increment the time counter (relative to media time)
             media_time = jnp.arange(t) + 1
 
             # text time must equal media time if only attending to most immediate image
@@ -147,16 +162,16 @@ class MaskedCrossAttention(nn.Module):
             mask_op = jnp.eq if only_attend_immediate_media else jnp.ge
 
             text_to_media_mask = mask_op(rearrange(text_time, 'b i -> b 1 i 1'), repeat(media_time, 'j -> 1 1 1 (j m)', m = m))
-            sim = sim.masked_fill(~text_to_media_mask, -jnp.finfo(sim.dtype).max)
+            sim = masked_fill_(sim, ~text_to_media_mask, -jnp.finfo(sim.dtype).max)
 
-        sim = sim - jnp.amax(sim, axis = -1, keepdims = True).detach()
+        sim = sim - jnp.amax(sim, axis = -1, keepdims = True)
         attn = nn.softmax(sim, axis = -1)
 
-        if exists(media_locations) and self.only_attend_immediate_media:
+        if exists(media_locations) and only_attend_immediate_media:
             # any text without a preceding media needs to have attention zeroed out
             text_without_media_mask = text_time == 0
             text_without_media_mask = rearrange(text_without_media_mask, 'b i -> b 1 i 1')
-            attn.masked_fill(text_without_media_mask, 0.)
+            masked_fill_(attn, text_without_media_mask, 0.)
 
         out = einsum('... i j, ... j d -> ... i d', attn, v)
         out = rearrange(out, 'b h n d -> b n (h d)')
@@ -178,11 +193,41 @@ class GatedCrossAttentionBlock(nn.Module):
     ):
 
         attn = MaskedCrossAttention(dim = self.dim, dim_head = self.dim_head, heads = self.heads, only_attend_immediate_media = self.only_attend_immediate_media)
-        attn_gate = nn.Parameter(torch.tensor([0.]))
+        attn_gate = jnp.array([0.])
 
         ff = FeedForward(self.dim, mult = self.ff_mult)
-        ff_gate = nn.Parameter(torch.tensor([0.]))
+        ff_gate = jnp.array([0.])
 
         x = attn(x, media, media_locations = media_locations) * jnp.tanh(attn_gate) + x
         x = ff(x) * jnp.tanh(ff_gate)  + x
         return x
+
+if __name__ == '__main__':
+
+    import numpy as np
+
+    key = jax.random.PRNGKey(0)
+
+    medias = jax.random.normal(key, (1, 2, 256, 1024))
+
+    perceive = PerceiverResampler(
+        dim = 1024,
+        depth = 2,
+        dim_head = 64,
+        heads = 8,
+        num_latents = 64,    # the number of latents to shrink your media sequence to, perceiver style
+        num_time_embeds = 4  # say you have 4 images maximum in your dialogue
+    )
+    
+    init_rngs = {'params': jax.random.PRNGKey(1), 
+                'latents': jax.random.PRNGKey(2), 
+                'time_pos_emb': jax.random.PRNGKey(3)}
+
+    params = perceive.init(init_rngs, medias)
+    output = perceive.apply(params, medias, rngs=init_rngs)
+    print(output.shape)
+
+    n_params_flax = sum(
+        jax.tree_leaves(jax.tree_map(lambda x: np.prod(x.shape), params))
+    )
+    print(f"Number of parameters in Flax model: {n_params_flax}")
